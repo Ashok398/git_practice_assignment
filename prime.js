9da4e2ad1f934f5bdc0b9f12a6e3523bd6e99108
Ashok Sharma <sharmaashutosh530@gmail.com>
2023-01-08 13:24:28 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,13 @@
+let number=7;
+let count=0;
+
+for( let i=1; i<=7; i++){
+if(7%i!==0){
+count++;
+}
+}
+if(count==2){
+console.log(number,"prime number")
+}
+else{ console.log(number,"not a prime number")
+}
